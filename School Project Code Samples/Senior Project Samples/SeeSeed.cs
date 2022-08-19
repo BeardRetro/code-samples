b8efa3425ecc957a8db385db6b3ab6a1519e7b9e
Daniel Hassett <danielmhassett@hotmail.com>
2022-08-18 20:48:32 -0400

    Initial commit

---
+++ School Project Code Samples/Senior Project Samples/SeeSeed.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//This is a spell that stops an enemy in their tracks for a certain period of time
+public class SeeSeed : MonoBehaviour 
+{
+	public float spellTimeInSeconds = 4.0f;
+    float enemySpeed;
+
+    //Activates the spell when an enemy walks into the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+
+            StartCoroutine(StopEnemy(other.gameObject));
+
+            //Destroys the spell after a certain duration
+            Invoke("DestroySpell", spellTimeInSeconds);
+        }
+    }
+
+    //Gets the speed of the enemy's NavMesh component, sets it to 0, and then restores it to the original speed after a duration
+    IEnumerator StopEnemy(GameObject other)
+    {
+        float secondsToFreeze = spellTimeInSeconds;
+
+        //Set enemy speed to enemy normal speed
+        enemySpeed = other.GetComponent<Enemy>().speed;
+
+        //Stop enemy
+        other.GetComponent<Enemy>().speed = 0;
+        yield return new WaitForSeconds(secondsToFreeze - 0.3f);
+
+        //Reset enemy speed to enemy normal speed
+        other.GetComponent<Enemy>().speed = enemySpeed;
+        
+    }
+
+	private void DestroySpell()
+	{
+        Destroy(gameObject);
+	}
+}
---
+++ School Project Code Samples/Survival Samples/HealthPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour 
+{
+	public PlayerHealth player;
+	public AudioSource pickupAudio;
+
+	void Start () 
+	{
+		player = FindObjectOfType<PlayerHealth> ();
+		pickupAudio = GameObject.Find ("Pickup Audio").GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("Player"))
+		{
+			player.currentHealth += 25;
+
+			if (player.currentHealth > 100)
+			{
+				player.currentHealth = 100;
+			}
+			player.healthSlider.value = player.currentHealth;
+			pickupAudio.Play ();
+
+			Destroy (gameObject);
+		}
+	}
+}
